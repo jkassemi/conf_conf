ff1153addd61782e46d18ea34df94f929e06c55e
James Kassemi <jkassemi@gmail.com>
2014-06-18 16:14:01 -0600

    Adds general configuration object
    Probably the better way to use the system, since Rails.configuration
    is _generally_ for Rails-specific application settings.

--- spec/conf_conf_spec.rb
+++ spec/conf_conf_spec.rb
@@ -3,51 +3,65 @@ require 'conf_conf'
 module Rails; end;
 
 describe ConfConf do
-  let(:configuration){ double() }
-  before { allow(Rails).to receive(:configuration).and_return(configuration) }
+  context "#configuration" do
+    it "sets a value from the environment" do
+      ENV["TEST_KEY"] = "hey"
 
-  it "sets a value from the environment" do
-    ENV["TEST_KEY"] = "hey"
-    expect(configuration).to receive(:test_key=).with("hey")
+      configuration = ConfConf.configuration {
+        config :test_key 
+      }
 
-    ConfConf.rails_configuration do
-      config :test_key
+      expect(configuration.test_key).to eq("hey")
     end
   end
 
-  it "sets the default value when key not present" do
-    expect(configuration).to receive(:key_not_present=).with("hey")
+  context "#rails_configuration" do
+    let(:configuration){ double() }
+    before { allow(Rails).to receive(:configuration).and_return(configuration) }
 
-    ConfConf.rails_configuration do
-      config :key_not_present, default: "hey"
-    end
-  end
+    it "sets a value from the environment" do
+      ENV["TEST_KEY"] = "hey"
+      expect(configuration).to receive(:test_key=).with("hey")
 
-  it "sets value from specified environment key" do
-    ENV["TEST_KEY"] = "hey"
-    expect(configuration).to receive(:other_key=).with("hey")
-
-    ConfConf.rails_configuration do
-      config :other_key, from: "TEST_KEY"
-    end
-  end
-
-  it "throws an exception when required key not present" do
-    expect {
       ConfConf.rails_configuration do
-        config :key_not_present
+        config :test_key
       end
-    }.to raise_error(ConfConf::MissingConfigurationValueError)
-  end
+    end
 
-  it "evaluates a block to establish the actual configuration value" do
-    ENV["INTEGER_VALUE"] = "2"
+    it "sets the default value when key not present" do
+      expect(configuration).to receive(:key_not_present=).with("hey")
 
-    expect(configuration).to receive(:integer_value=).with(2)
+      ConfConf.rails_configuration do
+        config :key_not_present, default: "hey"
+      end
+    end
 
-    ConfConf.rails_configuration do
-      config :integer_value do |value|
-        value.to_i
+    it "sets value from specified environment key" do
+      ENV["TEST_KEY"] = "hey"
+      expect(configuration).to receive(:other_key=).with("hey")
+
+      ConfConf.rails_configuration do
+        config :other_key, from: "TEST_KEY"
+      end
+    end
+
+    it "throws an exception when required key not present" do
+      expect {
+        ConfConf.rails_configuration do
+          config :key_not_present
+        end
+      }.to raise_error(ConfConf::MissingConfigurationValueError)
+    end
+
+    it "evaluates a block to establish the actual configuration value" do
+      ENV["INTEGER_VALUE"] = "2"
+
+      expect(configuration).to receive(:integer_value=).with(2)
+
+      ConfConf.rails_configuration do
+        config :integer_value do |value|
+          value.to_i
+        end
       end
     end
   end
